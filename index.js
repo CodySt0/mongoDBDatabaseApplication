e91dfc93a357070a4e7b725653562e3e601baf87
CodySt0 <Codistgeorge@gmail.com>
2024-07-25 14:51:01 -0700

    indexjs

---
+++ index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require(body - parser);
+
+const app = express();
+const port = 5004;
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/mydatabase", {
+  userNewUrlParser: true,
+  useInifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.listen(port, () => {
+  console.log("Server running at localhost:${port}/");
+});
